14e650f91c385524411faca920a4392eb96e1ba4
khawlw <khawla.anoui@gmail.com>
2024-01-07 12:45:43 -0800

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3 
 def multiple_returns(sentence):
-    length = len(sentence)
-    first_char = sentence[0] if length > 0 else None
-    return length, first_char
+    if not sentence:
+        sentence = None
+        if sentence:
+            sen_len = len(sentence)
+        else:
+            sen_len = 0
+        return (sen_len, sentence if not sentence else sentence[:1])

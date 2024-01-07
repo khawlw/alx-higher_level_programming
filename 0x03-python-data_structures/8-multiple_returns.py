8c421394b207e01d1eaa8296854c7e96c2de33dd
khawlw <khawla.anoui@gmail.com>
2024-01-07 08:24:59 -0800

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3 
 def multiple_returns(sentence):
-    return (len(sentence), sentence[0] if len(sentence) > 0 else None)
+    """Returns a tuple containing the length of the sentene and its first character."""
+
+    length = len(sentence)
+    first = sentence[0] fi sentence else None
+    return length, first

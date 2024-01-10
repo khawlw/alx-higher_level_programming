12117e696129118ca3343e62a2fcc9933215ecee
khawlw <khawla.anoui@gmail.com>
2024-01-10 13:38:27 -0800

    0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-<<<<<<< HEAD
     return list(map(lambda submat: list(map(lambda e: e**2, submat)), matrix))
-=======
-return list(map(lambda submat: list(map(lambda e: e**2, submat)), matrix))
->>>>>>> 1a0d6a9af918c7784472afbbd9305617c4d36ca8

22281c585c678a73e613ad8b30c9f25e754e9549
khawlw <khawla.anoui@gmail.com>
2024-01-23 13:06:02 -0800

    0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""square module."""
+Square = __import__('0-square').Square
 
-class square:
-    """Defines a square."""
-    pass
+my_square = Square()
+print(type(my_square))
+print(my_square.__dict__)

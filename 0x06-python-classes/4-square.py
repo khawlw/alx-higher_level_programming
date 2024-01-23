4a55e531b521b54073c94a8ba30cfc59795f12c1
khawlw <khawla.anoui@gmail.com>
2024-01-23 14:00:08 -0800

    4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -17,7 +17,7 @@ class Square:
         @property
         def size(self):
             """Get/set the current size of the square."""
-            return (self.__size)
+            return self.__size
 
         @size.setter
         def size(self, value):
@@ -29,4 +29,4 @@ class Square:
 
             def area(self):
                 """Return the current area of the square."""
-                return (self.__size * self.__size)
+                return self.__size * self.__size

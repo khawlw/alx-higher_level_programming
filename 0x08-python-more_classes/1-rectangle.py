c1cffb9dbee62ef483251d05eb668bad2239ac4d
khawlw <khawla.anoui@gmail.com>
2024-01-29 12:51:44 -0800

    1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -3,30 +3,37 @@
 
 
 class Rectangle:
+    """Represent a rectangle."""
+
     def __init__(self, width=0, height=0):
+        """Initialize a new Rectangle.
+
+        Args:
+        width (int): The width of the new rectangle.
+        height (int): The height of the new rectangle.
+        """
         self.width = width
         self.height = height
 
-    @property
-    def width(self):
-        return self.__width
+        @property
+        def width(self, value):
+            if not isinstance(value, int):
+                raise TypeError("width must be an integer")
+            if value < 0:
+                raise ValueError("width must be >= 0")
+            self.__width = value
 
-    @width.setter
-    sef width(self, value):
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        elif valye < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
+        @property
+        def height(self):
+            """Get/set the height of the rectange."""
+            return self.__height
 
-    @property
-    def height(self):
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        elif value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
+        @height.setter
+        def height(self, value):
+            if not isinstance(value, int):
+                raise TypeError("height must be an integer")
+            if value < 0:
+                raise TypeError("height must be an integer")
+            if value < 0:
+                raise ValueError("height must be >= 0")
+            self.__height = value
--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-
 class Rectangle:
     """Representation of a rectangle"""
     def __init__(self, width=0, height=0):

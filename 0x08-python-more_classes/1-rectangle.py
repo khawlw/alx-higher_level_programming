382cf4f4de8dc395714b0e144afa59042c6a947e
khawlw <khawla.anoui@gmail.com>
2024-01-29 08:57:07 -0800

    1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -17,26 +17,38 @@ class Rectangle:
 
         @property
         def width(self):
-            """Get/set the width of the rectangle."""
+            """getter for the private instance attribute width"""
             return self.__width
-        
+
         @width.setter
         def width(self, value):
-            if not isinstance(value, int):
-                raise TypeError("width must be a integer")
+            """setter for the private instance attribute width"""
+            if type(value) is not int:
+                raise TypeError("width must be an integer")
             if value < 0:
                 raise ValueError("width must be >= 0")
             self.__width = value
 
         @property
         def height(self):
-            """Get/set the height of the rectangle."""
+            """getter for the private instance attribute height"""
             return self.__height
 
         @height.setter
         def height(self, value):
-            if not isinstance(value, int):
-                raise TypeError("height muste be an integer")
-            if value < 0
-            raise ValueError("height must be >= 0")
-        self.__height = value
+            """setter for the private instance attribute height"""
+            if type(value) is not int:
+                raise TypeError("height must be an integer")
+            if value < 0:
+                raise ValueError("height must be >= 0")
+            self.__height = value
+
+        def area(self):
+            """return the area of the rectangle"""
+            return self.__width * self.__height
+
+        def perimeter(self):
+            """return the perimeter of the rectangle"""
+            if self.__width == 0 or self.__height == 0:
+                return 0
+            return (self.__width * 2) * (self.__height *2)

b80013eaa3648ff92bd214c5dbb7bd861874be9c
khawlw <khawla.anoui@gmail.com>
2024-02-06 13:39:29 -0800

    4-from_json_string.py

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,20 +1,9 @@
 #!/usr/bin/python3
-"""Defines a text file insertion function."""
+# 6-from_json_string.py
+"""Defines a JSON-to-object function."""
+import json
 
 
-def append_after(filename="", search_string="", new_string=""):
-    """Insert text after each line containing a given string in a file.
-
-    Args:
-    filename (str): The name of the file.
-    search_string (str): The string to search for within the file.
-    new_string (str): The string to insert.
-    """
-    text = ""
-    with open(filename) as r:
-        for line in r:
-            text += line
-            if search_string in line:
-                text += new_string
-                with open(filename, "w") as w:
-                     w.write(text)
+def from_json_string(my_str):
+    """Return the Python object representation of a JSON string."""
+    return json.loads(my_str)

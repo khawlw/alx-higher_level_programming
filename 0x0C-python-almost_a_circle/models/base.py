9641bf4287fa8114e42510676012b216c537a71c
khawlw <khawla.anoui@gmail.com>
2024-02-13 08:43:17 -0800

    a

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -44,3 +44,13 @@ class Base:
             new = None
         new.update(**dictionary)
         return new
+
+    @classmethod
+    def load_from_file(cls):
+        """Loads stringfrom file and unjsonidies."""
+        from os import path
+        file = "{}.json".format(cls.__name__)
+        if not path.isfile(file):
+            return []
+        with open(file, "r", encoding="utf-8") as f:
+            return [cls.create(**d) for d in cls.rom_json_string(f.read())]

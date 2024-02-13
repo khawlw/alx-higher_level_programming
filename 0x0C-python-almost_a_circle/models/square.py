28d9d0428ec0781e48251440db2722ec636547c7
khawlw <khawla.anoui@gmail.com>
2024-02-13 13:02:57 -0800

    a

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -13,4 +13,14 @@ class Square(Rectangle):
     def __str__(self):
         """Returns string info about this square."""
         return '[{}] ({}) {}/{} - {}'.\
-                format(type(self.__name__, self.id, self.x, self.y, self.width)
+            format(type(self).__name__, self.id, self.x, self.y, self.width)
+
+    @property
+    def size(self):
+        """Size of this square."""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value

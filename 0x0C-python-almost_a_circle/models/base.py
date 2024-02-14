ab3292869f10b37bd8a552e70c2e1ea59f124287
khawlw <khawla.anoui@gmail.com>
2024-02-14 03:35:45 -0800

    a

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """Module for Base class."""
-from json import dumps, loads
 
 
 class Base:
@@ -15,50 +14,3 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-
-    @staticmethod
-    def to_json_string(list_dictionaries):
-        """Jsonifies a dictionary so it's quite rightly and longer."""
-        if list_dictionaries is None or not list_dictionaries:
-            return "[]"
-        else:
-            return dumps(list_dictionaries)
-
-    @staticmethod
-    def from_json_string(json_string):
-        """Unjsonifies a dictionary."""
-        if json_string is None or not json_string:
-            return []
-        return loads(json_string)
-
-    @classmethod
-    def save_to_file(cls, list_objs):
-        """Saves jsonified object to file."""
-        if list_objs is not None:
-            list_objs = [o.to_dictionary() for o in list_objs]
-        with open("{}.json".format(cls.__name__), "w", encoding="utf-8") as f:
-            f.write(cls.to_json_string(list_objs))
-
-    @classmethod
-    def create(cls, **dictionary):
-        """Loads instance from dictionary."""
-        from models.rectangle import Rectangle
-        from models.square import Square
-        if cls is Rectangle:
-            new = Rectangle(1, 1)
-        elif cls is Square:
-            new = Square(1)
-        else:
-            new = None
-        new.update(**dictionary)
-        return new
-
-    @classmethod
-    def load_from_file(cls):
-        """Loads stringfrom file and unjsonidies."""
-        from os import path
-        file = "{}.json".format(cls.__name__)
-        if not path.isfile(file):
-            return []
-        with open(file, "r", encoding="utf-8") as f:
-            return [cls.create(**d) for d in cls.rom_json_string(f.read())
